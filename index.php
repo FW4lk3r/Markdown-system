d50a7e57e83328d14c670c3f9d6f2fceccf84e43
Filipe Ferreira <filipesferreira94@gmail.com>
2018-11-22 19:24:50 +0000

    add connection to database

--- index.php
+++ index.php
@@ -1,19 +1,32 @@
 <?php 
 
-    //data of the database
-
     //table
 
     //inserting data to database
 
     //connection to database
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
 
     //verify the form
     if($_POST['submit']){
 
         //get the text
+        $title = htmlspecialchars($_POST['title']);
+        $comment = htmlspecialchars($_POST['comment']);
 
         //insert into database
+        $query = "INSERT INTO XXXXXX () VALUES ();";
+        mysqli_query($conn, $query);
 
         //redirect
         header("Location: index.php");
@@ -34,7 +47,7 @@
     
     <form action="#" method="post">
         <input type="text" name="title" placeholder="Title">
-        <textarea name="teste" id="teste" cols="30" rows="10" placeholder="Your comment"></textarea>
+        <textarea name="comment" id="teste" cols="30" rows="10" placeholder="Your comment"></textarea>
         <button type="submit">Send your comment</button>
     </form>
 
