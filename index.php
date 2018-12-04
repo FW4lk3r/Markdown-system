031539681b60c7cb5e24d176d62d490f0bda7b1e
Filipe Ferreira <filipeferreira@MacBook-Pro-de-Filipe.local>
2018-12-04 10:17:34 +0000

    change connection data

--- index.php
+++ index.php
@@ -6,16 +6,19 @@
 
     //connection to database
     $servername = "localhost";
-    $username = "username";
-    $password = "password";
-
+    $username = "root";
+    $password = "";
+    $dbname = "markdown-system";
+    
     // Create connection
-    $conn = new mysqli($servername, $username, $password);
-
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-    } 
+    }
+    echo "Connected successfully";
+
 
     //verify the form
     if($_POST['submit']){
